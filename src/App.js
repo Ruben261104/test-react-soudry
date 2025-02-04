c496bbd110dbbce02864adb6079c21494aa00097
Ruben <rub.attal@gmail.com>
2025-02-04 12:29:26 +0100

    bdd test 12

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Ruben
+          Learn React Ruben Attal
         </a>
       </header>
     </div>
