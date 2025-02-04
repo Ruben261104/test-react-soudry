39d65210979e903d9d7b3fd0caf9c046a104d628
Ruben <rub.attal@gmail.com>
2025-02-04 11:06:46 +0100

    bdd test 2

--- backup_restore.sh
+++ backup_restore.sh
@@ -4,23 +4,9 @@
   DB_USER="root"
   DB_PASSWORD="root"
   DB_NAME="mydatabase"
-  BACKUP_FILE="db_backup.sql"
+  BACKUP_FILE="init.sql"
 
   echo "📦 Sauvegarde de la base de données..."
   docker exec $DB_CONTAINER mysqldump -u$DB_USER -p$DB_PASSWORD $DB_NAME > $BACKUP_FILE
-
-  echo "🛑 Arrêt et suppression des conteneurs..."
-  docker-compose down
-
-  echo "🚀 Nettoyage Docker..."
-  docker system prune -af
-
-  echo "🔧 Reconstruction des conteneurs..."
-  docker-compose build
-  docker-compose up -d
-
-  echo "📥 Réimportation de la base de données..."
-  sleep 10 # Laisse le temps à MySQL de redémarrer
-  docker exec -i $DB_CONTAINER mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME < $BACKUP_FILE
 
   echo "✅ Déploiement terminé !"
