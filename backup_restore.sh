9819947ab7ca9487af51cd2e01df0956a2ff3749
Ruben <rub.attal@gmail.com>
2025-02-04 15:20:29 +0100

    Test Finale SH

--- backup_restore.sh
+++ backup_restore.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=$PATH:/usr/bin
 
 DB_CONTAINER="mysql-db"
 DB_USER="root"
@@ -6,6 +7,9 @@ DB_PASSWORD="root"
 DB_NAME="mydatabase"
 BACKUP_FILE="backup.sql"
 PATH="../../backups"
+
+sudo usermod -aG docker $USER
+newgrp docker
 
 echo "📥 Réimportation de la base de données..."
 docker exec -i $DB_CONTAINER mysql -u$DB_USER -p$DB_PASSWORD $DB_NAME < $PATH"/"$BACKUP_FILE
--- backup_save.sh
+++ backup_save.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export PATH=$PATH:/usr/bin
 
 DB_CONTAINER="mysql-db"
 DB_USER="root"
@@ -6,6 +7,9 @@ DB_PASSWORD="root"
 DB_NAME="mydatabase"
 BACKUP_FILE="backup.sql"
 PATH="../../backups"
+
+sudo usermod -aG docker $USER
+newgrp docker
 
 echo "📦 Sauvegarde de la base de données..."
 docker exec $DB_CONTAINER mysqldump -u$DB_USER -p$DB_PASSWORD $DB_NAME > $PATH"/"$BACKUP_FILE
