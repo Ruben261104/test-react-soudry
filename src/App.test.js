9066e7bb63abe8c4d296cc56dd59d8bafefe615f
Ruben <rub.attal@gmail.com>
2025-02-04 09:46:10 +0100

    test Pour les tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,11 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument(); // Ce test fonctionne si "learn react" est présent
 });
 
+
+/*
 // Test qui va volontairement échouer
 test('Ce test doit échouer', () => {
   render(<App />);
   const errorElement = screen.getByText(/ce texte n'existe pas/i);
   expect(errorElement).toBeInTheDocument(); // Échouera car ce texte n'est pas dans App.js
-});
+});*/
